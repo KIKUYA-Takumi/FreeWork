8213f4da3db62984d9f006a1a8156af9f64dcbb6
KIKUYA-Takumi <uwasanoace@yahoo.co.jp>
2016-09-01 09:18:51 +0900

    modify file
    add function search

--- practice/report/views.py
+++ practice/report/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.template import RequestContext
+from django.db.models import Q
 
 from .models import Report, Comment
-from .forms import ReportForm, CommentForm
+from .forms import ReportForm, CommentForm, SearchForm
 
 
 # Create your views here.
@@ -24,9 +26,20 @@ def create_report(request):
         if form.is_valid():
             form.save()
             messages.success(request, '投稿を受け付けました!')
-            return redirect('report:index')
+            return render(request, 'report/index.html', {'form': form})
         else:
             messages.error(request, '入力内容に誤りがあります!')
-    return render(request, 'report/index.html')
+    return render(request, 'report/entry.html', {'form': form})
+
+
+def search(request):
+    form = SearchForm(request.GET or None)
+    if request.method == 'GET':
+        if form.is_valid():
+            reports = Report.objects.filter(Q(report_content__contains=form.cleaned_data['search_word']))
+            return render_to_response('report/search.html', {'form': form, 'reports': reports}, RequestContext(request))
+    else:
+        return render(request, 'report/search.html', {'form': form})
+
 
 
